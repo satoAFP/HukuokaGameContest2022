84eaf7dbf78447c263d61de2cc3efc4ba68d8385
DEV3-6\GA3 <h120201907@hiratagakuen.onmicrosoft.com>
2022-09-02 16:32:32 +0900

    スタートアップ完成

---
+++ HukuokaGameContest2022/Assets/sato/Script/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController controller;
+    Vector3 movedir = Vector3.zero;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+
+            movedir.y = Input.GetAxis("Vertical") * -5.0f;
+            movedir.x = Input.GetAxis("Horizontal") * 5.0f;
+
+            if (Input.GetButton("Jump"))
+            {
+                movedir.z = 10f;
+            }
+        }
+
+        movedir.z -= 20f * Time.deltaTime;
+
+        Vector3 globaldir = transform.TransformDirection(movedir);
+        controller.Move(globaldir * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            movedir.z = 0;
+        }
+    }
+}
